31d4a74ed1a359d92d7c47a142e12cde68129c19
colchang <colchang@iu.edu>
2020-04-29 20:58:17 -0700

    Altered host for heroku connection

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "burger",
+        host: "localhost",
         user: "root",
         password: "",
         database: "burgers_db"
